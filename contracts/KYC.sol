118e7c340e2384a5308103f96ac6fc07abdb4af8
Remix Workspace <>
2022-10-30 17:26:49 +0800

    KYC contract done

--- contracts/KYC.sol
+++ contracts/KYC.sol
@@ -14,18 +14,15 @@ contract KYC{
         string role;
         string firstName;
         string lastName;
-        // string country;
-        // string location;
-        // string email;
-        // string nric;
-        // string phoneNum;
-        // string nationality;
+        string country;
+        string location;
+        string nationality;
         // uint256 timestamp;
         // test git changes
         // test git changes - xianwei
     }
 
-    function onboarding(address payable userAddress, string memory role, string memory firstName, string memory lastName) public {
+    function onboarding(address payable userAddress, string memory role, string memory firstName, string memory lastName, string memory country, string memory location, string memory nationality) public {
         transactionCount +=1;
         // transactions.push(TransferFundsStruck(msg.sender, receiver, amount, message, block.timestamp));
         userDict[userAddress] = UserInfoStruct(userAddress, role, firstName, lastName);
@@ -34,10 +31,10 @@ contract KYC{
     
     }
 
-    function getUserInfo(address payable userAddress) public view returns(UserInfoStruct[] memory){
-        UserInfoStruct[] memory userInfo = new UserInfoStruct[](1);
-        userInfo[0] = userDict[userAddress];
-        return userInfo;
+    function getUserInfo(address payable userAddress) public view returns(UserInfoStruct memory){
+        // UserInfoStruct[] memory userInfo = new UserInfoStruct[](1);
+        // userInfo[0] = userDict[userAddress];
+        return userDict[userAddress];
     }
 
     function getTransactionCount() public view returns(uint256){
--- contracts/TransactionProcessing.sol
+++ contracts/TransactionProcessing.sol
@@ -1,15 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
+import "./KYC.sol";
 
 contract TransactionProcessing{
-
+    event CreateLog(string firstName, uint256 timestamp);
     address payable public sender;
     address payable public receiver;
+    address public kycAddress; 
+    KYC public kyc; 
 
-    constructor(address payable _receiver) payable {
+    constructor(address payable _receiver, address _kycAddress) payable {
         require(_receiver != address(0), "receiver = zero address");
         sender = payable(msg.sender);
         receiver = _receiver;
+        kycAddress = _kycAddress;
+        kyc = KYC(_kycAddress); 
+    }
+
+    function processing() public {
+        
+        //get sender userInfo
+        string memory firstName = kyc.getUserInfo(sender).firstName;
+        emit CreateLog(firstName, block.timestamp);
+
+        // check sender AML
     }
 
     function sendViaCall() public payable {
